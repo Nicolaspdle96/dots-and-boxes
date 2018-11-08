cfdd5506bf0146eaea59f203e3c14e01730b8442
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-11-08 02:23:39 -0400

    solved conflicts

--- app.rb
+++ app.rb
@@ -1,19 +1,25 @@
 require 'sinatra'
-require './lib/board'
+require './lib/player'
+
 
 class App < Sinatra::Base
     get '/' do
         erb:home
     end
 
-    post '/players' do
+    get '/players' do
         erb:players
+    end 
+
+    get '/game' do
+        @pl1 = Player.new(params[:player1])
+        @pl2 = Player.new(params[:player2])
+        erb:game
     end
 
     get '/pvsp' do  #player VS player
         board = Board.new(4)
         board.generateHTML() + board.generateCss()
-    end 
     
     run! if app_file == $0;
 end
