21cb6708ab032437ac34544c625ee4ac9304fbc9
Ina <inaeterovic>
2018-11-25 14:24:09 -0400

    Fill players names

--- features/player.feature
+++ features/player.feature
@@ -9,5 +9,14 @@ Feature: register players names
         And I fill the second "player2" with "Pedro"
         And I press the button "Jugar"
         Then I see the title "Game"
+
+    Scenario: register multiplayer names
+    Given the moreplayers page
+        And I fill the "player1" with "John"
+        And I fill the second "player2" with "Pedro"
+         And I fill the  third "player3" with "Mary"
+        And I fill the fourth "player4" with "Cristina"
+        And I press the button "Jugar"
+        Then I see the title "Game"
       
     
--- features/step_definitions/player_steps.rb
+++ features/step_definitions/player_steps.rb
@@ -1,6 +1,10 @@
 Given("the players page") do
     visit('/players')
   end
+
+Given("the moreplayers page") do
+    visit('/moreplayers')
+end
   
   Given("I fill the {string} with {string}") do |field, name|
     fill_in 'player1', :with => name
@@ -8,6 +12,14 @@ Given("the players page") do
   
   Given("I fill the second {string} with {string}") do |field, name|
     fill_in 'player2', :with => name
+  end
+
+  Given("I fill the  third {string} with {string}") do |field, name|
+   fill_in 'player3', :with => name
+  end
+  
+  Given("I fill the fourth {string} with {string}") do |field, name|
+    fill_in 'player4', :with => name
   end
   
   Then("I see the title {string}") do |title|
