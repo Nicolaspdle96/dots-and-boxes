f9c80b961dfbccaa92843f48e347caf51f438345
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2018-11-25 19:07:20 -0400

    should return the number5 with the name 'Jose' when all the players get better scores

--- lib/ranking.rb
+++ lib/ranking.rb
@@ -33,14 +33,53 @@ class Ranking
     end
 
     def sortList(player)
-        if player.score > @number1.score 
-            aux = @number1.name
+        if player.score >= @number1.score 
+            swamp(1,@number1)
             @number1 = player
+        else
+            if player.score >= @number2.score
+                swamp(2,@number2)
+                @number2 = player
             else
-                if player.score > @number2.score
-                    aux = @number2.name
-                    @number2 = player
+                if player.score >= @number3.score
+                    swamp(3,@number3)
+                    @number3 = player
+                else
+                    if player.score >= @number4.score
+                        swamp(4,@number4)
+                        @number4 = player
+                    else 
+                        if player.score >= @number5.score
+                            @number5 = player
+                        end
+                    end
                 end
+            end
+        end
+    end
+
+    def swamp(number,player)
+        case number 
+        when 1
+            aux = @number2
+            @number2 = player
+            aux2 = @number3
+            @number3 = aux
+            aux3 = @number4
+            @number4 = aux2
+            @number5 = aux3
+        when 2
+            aux2 = @number3
+            @number3 = player
+            aux3 = @number4
+            @number4 = aux2
+            @number5 = aux3
+        when 3
+            aux3 = @number4
+            @number4 = player
+            @number5 = aux3
+        when 4
+            @number5 = player
         end
     end
 
--- spec/ranking_spec.rb
+++ spec/ranking_spec.rb
@@ -28,4 +28,33 @@ describe Ranking do
         expect(@top5.number2.name).to eq 'Juan'
     end
 
+    it "should return the number1 with the name 'Juan' when he takes his place" do
+        @player1 = Player.new('Jose','red')
+        @player2 = Player.new('Juan','red')
+        @player1.score = 5
+        @player2.score = 10
+        @top5.sortList(@player1)
+        @top5.sortList(@player2)
+        expect(@top5.number1.name).to eq 'Juan'
+    end
+
+    it "should return the number5 with the name 'Jose' when all the players get better scores" do
+        @player1 = Player.new('Jose','red')
+        @player2 = Player.new('Juan','red')
+        @player3 = Player.new('Jorge','red')
+        @player4 = Player.new('Jeremias','red')
+        @player5 = Player.new('Jurguen','red')
+        @player1.score = 5
+        @player2.score = 10
+        @player3.score = 8
+        @player4.score = 14
+        @player5.score = 10
+        @top5.sortList(@player1)
+        @top5.sortList(@player2)
+        @top5.sortList(@player3)
+        @top5.sortList(@player4)
+        @top5.sortList(@player5)
+        expect(@top5.number5.name).to eq 'Jose'
+    end
+
 end
